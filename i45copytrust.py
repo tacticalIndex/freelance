3ff29eecd43ae6ff08008a40e6bd54208bb7f46d
IndexCoderong <148581158+tacticalIndex@users.noreply.github.com>
2025-10-13 23:01:30 -0500

    Update i45copytrust.py

--- i45copytrust.py
+++ i45copytrust.py
@@ -8,10 +8,36 @@ def account_join():
     serverid = request.args.get("serverid")
     headers = request.headers
     authorization = headers.get('Authorization')
+    setup = False
 
     if authorization is None:
         return "Authorization Header is not found.", 401
     
-    if authorization == process.env.accountjoinheader:
-        try: 
-            
+    if authorization == process.env.accountjoinheader and serverid is not None:
+        pyautogui.click(x=0, y=0) # Placeholder, servers tab
+        pyautogui.click(x=0, y=0) # Placeholder, search bar
+        pyautogui.write(serverid) 
+        pyautogui.press('enter')
+        pyautogui.click(x=0, y=0) # Placeholder, whatever team (who knows what)
+        pyautogui.click(x=0, y=0) # Placeholder, spawn on selected team
+
+        
+        payload = {
+            "readyForExecution": true,
+            "message": "i55 ready for service 👽"
+        }
+        return jsonify(payload), 200
+    elif serverid is None:
+        payload = {
+            "readyForExecution": false,
+            "message": "i55 not ready for service 🥀",
+            "errorMessage": "Argument: `serverid` was missing from the request."
+        }
+        return jsonify(payload), 400
+    elif authorization != process.env.accountjoinheader:
+        payload = {
+            "readyForExecution": false,
+            "message": "i55 not ready for service 🥀",
+            "errorMessage": "Authorization header is not valid.
+        }
+        return jsonify(payload), 400
